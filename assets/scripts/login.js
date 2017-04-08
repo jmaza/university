b01dce51c4b978ca2083734318b058efbce0e85b
Jose Maza <maza_jose@hotmail.com>
2017-04-07 21:28:47 -0600

    login works and api first steps

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,12 +1,18 @@
 'use strict';
 
-document.onreadystatechange = function () {
-
-    if (document.readyState === 'complete') {
-        document.getElementById('btnLogin').addEventListener('click', function (event) {
-
-            event.preventDefault();
-            console.log('Login Button Clicked');
-        });
-    }
-};
+$(document).ready(function(){
+      $("#btnLogin").click(function(e){
+          e.preventDefault();
+        $.ajax({type: "POST",
+                url: "/login",
+                contentType: "application/json",
+                data: JSON.stringify({ username: $("#username").val(), password: $("#password").val() }),
+                success:function(result){
+                    window.location.href = "/home"
+                },
+                error: function(result) {
+                    alert('error');
+                }
+            });
+      });
+});
